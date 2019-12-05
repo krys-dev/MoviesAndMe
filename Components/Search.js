4c6bbef8c5b0eb6fba0ee327f5f27d74609620ed
krys-dev <djoumesstonf@gmail.com>
2019-12-05 13:08:42 +0100

    Version 1

---
+++ Components/Search.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {StyleSheet, View, Button, TextInput} from 'react-native'
+
+
+
+class Search extends React.Component{
+    render() {
+        return (
+            <View style={{marginTop: 20}}>
+                <TextInput style={styles.textinput} placeholder="Titre du film"/>
+                <Button title="Rechercher" onPress={() => {}}/>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create(
+    {
+        textinput : {
+            marginLeft: 5,
+            marginRight: 5,
+            height: 50,
+            borderColor: '#000000',
+            borderWidth: 1,
+            paddingLeft: 5
+        }
+    });
+
+export default Search
